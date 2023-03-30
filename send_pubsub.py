756dc1ba9b0d9ada9914070332fa9080e9179bc4
Shamaneiro <jakub.struczynski@gmail.com>
2023-03-30 16:49:36 +0200

    new detect functions

--- send_pubsub.py
+++ send_pubsub.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import pytz
 
 project_id = "ai-store-heatmapping-incubator"
-topic_id = "coral-topic-demo2"
+topic_id = "coral-topic-demo"
 
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, topic_id)
@@ -16,4 +16,4 @@ return_msg['zone1'] = 1
 return_msg['zone2'] = 0
 data_str = str(return_msg)
 
-future = publisher.publish(topic_path, data_str.encode("utf-8"))
+future = publisher.publish(topic_path, "kuba".encode("utf-8"))
