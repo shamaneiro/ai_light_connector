114ccc844d141fc7c7b211bdc1d85a235af24635
Shamaneiro <jakub.struczynski@gmail.com>
2023-03-30 13:58:50 +0200

    new detect functions

--- detect_print.py
+++ detect_print.py
@@ -71,6 +71,7 @@ def main():
 
 
     cap = cv2.VideoCapture(args.camera_idx)
+    cap.set(cv2.CAP_PROP_FPS, 2)
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -98,16 +99,17 @@ def append_objs_to_img(cv2_im, inference_size, objs, labels):
     scale_x, scale_y = width / inference_size[0], height / inference_size[1]
 
     for obj in objs:
-        bbox = obj.bbox.scale(scale_x, scale_y)
-        x0, y0 = int(bbox.xmin), int(bbox.ymin)
-        x1, y1 = int(bbox.xmax), int(bbox.ymax)
+        if int(obj.id) == 0:
+            bbox = obj.bbox.scale(scale_x, scale_y)
+            x0, y0 = int(bbox.xmin), int(bbox.ymin)
+            x1, y1 = int(bbox.xmax), int(bbox.ymax)
 
-        percent = int(100 * obj.score)
-        label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
+            percent = int(100 * obj.score)
+            label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
 
-        cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
-        cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
-                             cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+            cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
+            cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
     return cv2_im
 
 def print_detected_objects(cv2_im, inference_size, objs, labels):
